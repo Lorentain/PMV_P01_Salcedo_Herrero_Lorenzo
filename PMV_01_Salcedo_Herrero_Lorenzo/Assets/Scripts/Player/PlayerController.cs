983b747984581a06ec73c25eaa9adc7d4e419d5a
Lorenzo Herrero Salcedo <lorentain2@gmail.com>
2024-09-26 14:53:15 +0200

    Contador de vida del player, contador de moneda y decoración

--- PMV_01_Salcedo_Herrero_Lorenzo/Assets/Scripts/Player/PlayerController.cs
+++ PMV_01_Salcedo_Herrero_Lorenzo/Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    [Tooltip("Referencia al Rigidbody2D")] 
+    [Tooltip("Referencia al Rigidbody2D")]
     [SerializeField] private Rigidbody2D rb;
 
-    [Tooltip("Referencia a la fuerza del salto del jugador")] 
+    [Tooltip("Referencia a la fuerza del salto del jugador")]
     [SerializeField] private float jumpForce;
 
-    [Tooltip("Referencia a la fuerza de caida del salto del jugador")] 
+    [Tooltip("Referencia a la fuerza de caida del salto del jugador")]
     [SerializeField] private float fallForce;
 
-    [Tooltip("Referencia a la velocidad el jugador")] 
+    [Tooltip("Referencia a la velocidad el jugador")]
     [SerializeField] private float speed;
+
+    public int playerHP = 3;
+
+    public int counterCoins = 0;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -46,15 +52,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        if(other.collider.CompareTag("Coin")) {
+    // Colisión contra monedas
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Coin"))
+        {
             Destroy(other.gameObject);
+            counterCoins++;
+            Debug.Log("Monedas obtenidas " + counterCoins);
+        }
+
+        if (other.collider.CompareTag("Spike"))
+        {
+            Destroy(gameObject);
+            Debug.Log("MUERTO");
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Enemy")) {
-            Destroy(gameObject);
+    // Colisión contra enemigos
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            if (playerHP == 1)
+            {
+                Destroy(gameObject);
+                Debug.Log("MUERTO");
+            }
+            else
+            {
+                playerHP--;
+                Debug.Log("Vidas restantes " + playerHP);
+            }
         }
     }
 }
