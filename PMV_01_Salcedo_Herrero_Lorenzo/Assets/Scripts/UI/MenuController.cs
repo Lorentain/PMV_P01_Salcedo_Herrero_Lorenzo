4d768dede78a90abaf60001eecfc60ff4ef3255d
Lorenzo Herrero Salcedo <lorentain2@gmail.com>
2024-10-17 01:12:46 +0200

    Quitado los buttonExiteController y buttonPlayAgainController

--- PMV_01_Salcedo_Herrero_Lorenzo/Assets/Scripts/UI/MenuController.cs
+++ PMV_01_Salcedo_Herrero_Lorenzo/Assets/Scripts/UI/MenuController.cs
@@ -1,13 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
 
-    [SerializeField] private GameObject initialWarning;
+    [Tooltip("")]
+    [SerializeField] private GameObject initialWarning; // Canvas aviso al iniciar el juego
 
-    public void AceptarComienzo() {
+    [SerializeField] private string linkSurvey; // Link de la encuesta
+
+    [SerializeField] private string LevelToLoad;
+
+    public void AceptarComienzo()
+    {
         initialWarning.gameObject.SetActive(false);
+    }
+
+    public void AbrirEncuesta()
+    {
+        Application.OpenURL(linkSurvey);
+    }
+
+    public void ExitGame()
+    {
+        // Si estás en el editor de Unity, esto parará la ejecución del juego.
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        // Esto cerrará la aplicación si está ejecutándose fuera del editor.
+        Application.Quit();
+        #endif
+    }
+
+    public void NewGameLevel()
+    {
+        SceneManager.LoadScene(LevelToLoad);
     }
 }
